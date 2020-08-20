ea0405327860af697ff2e8d5786e5f840f876779
Javier Ubillos <javier@ubillos.org>
2020-08-20 23:16:46 +0200

    Add support for custom types for ExpoPushMessage.data (Map<String, T>)
    The default usage of PushClient and ExpoPush message remains.
    The default type for the data member is now Map<String, Object>, which is closer to the
    expo API documentation.
    https://docs.expo.io/push-notifications/sending-notifications/#message-request-format

--- src/test/java/io/github/jav/exposerversdk/PushClientTestCustomData.java
+++ src/test/java/io/github/jav/exposerversdk/PushClientTestCustomData.java
@@ -14,11 +14,10 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-class PushClientTest {
+class PushClientTestCustomData {
 
     @Test
     public void apiBaseUrlIsOverridable() throws MalformedURLException {
