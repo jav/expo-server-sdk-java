dc5aab252e1aab0dfeade1b55207caa731ae62aa
Douglas Robert Wallin <wallind@mail.gvsu.edu>
2021-08-27 12:00:13 -0400

    fixed formatting to use spaces for modification

--- src/main/java/io/github/jav/exposerversdk/enums/TicketError.java
+++ src/main/java/io/github/jav/exposerversdk/enums/TicketError.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public enum TicketError {
         @JsonProperty("DeviceNotRegistered")
         DEVICENOTREGISTERED("DeviceNotRegistered"),
-				@JsonProperty("InvalidCredentials")
+        @JsonProperty("InvalidCredentials")
         INVALIDCREDENTIALS("InvalidCredentials");
 
         private String error;
